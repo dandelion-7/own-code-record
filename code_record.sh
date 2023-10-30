192f0304f572ffd2dfc1805d6a5d22fd2097cb4f
张高溥 <zhanggaopu@zhanggaopudeMacBook-Pro.local>
2023-10-30 10:25:25 +0800

    updated code_record

--- code_record.sh
+++ code_record.sh
@@ -1,4 +1,15 @@
+# common code record
+
+## Bash commands
+
+### scp transfering files
 ```bash
-### scp transfering from local to remote clusters
+# scp transfering from local to remote clusters
 scp -P 20606 uhgp.faa xiangyujia@166.111.153.65:/home/xiangyujia
 ```
+
+### us `ls` to list all files and use comma to separate
+```bash
+ls ${subject}_*_dehosted.2.fastq | xargs | sed 's/ /,/g  
+```
+#`ls` would directly set new lines in the output, while `sed` takes each line to replace syntax, so the "\n" can't be replace directly with `sed`. `xargs` can represent the previous output and convert it to a thorough line for `sed`
